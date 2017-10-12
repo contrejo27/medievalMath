2f41bc55c4f667940f4c43a0ec82a42c6b9c90a9
contrejo27 <contrejo27@gmail.com>
2017-10-12 15:24:37 -0400

    added new art assets,  added door health bar

---
+++ Castle Math/Assets/GameBarCreationSystem/Scripts/ExampleGUIAspectsController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+class ExampleGUIAspectsController : MonoBehaviour
+{
+    private ExperienceSystem exp_bar;
+    private int last_level = 1;
+
+    private HealthSystem health_bar;
+    private ManaSystem mana_bar;
+
+    private GlobeBarSystem globe_test_bar;
+
+    public Rect HealthBarDimens;
+    public bool VerticleHealthBar;
+    public Texture HealthBubbleTexture;
+    public Texture HealthTexture;
+    public float HealthBubbleTextureRotation;
+
+    public void Start()
+    {
+        health_bar = new HealthSystem(HealthBarDimens, VerticleHealthBar, HealthBubbleTexture, HealthTexture, HealthBubbleTextureRotation);
+
+        health_bar.Initialize();
+
+    }
+
+    public void OnGUI()
+    {
+        health_bar.DrawBar();
+        
+    }
+
+	public void updateHealth(int healthLoss){
+		health_bar.IncrimentBar(healthLoss * -1);
+	}
+	
+	
+    public void Update()
+    {
+        health_bar.Update();
+
+    }
+}
