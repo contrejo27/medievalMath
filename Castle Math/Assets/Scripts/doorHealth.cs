ad98eca5889fba1e71372ac30a44f2f95807a57e
Hannah <hholman1902@gmail.com>
2017-10-15 15:57:02 -0400

    Destroy gate on Health < 0

--- Castle Math/Assets/Scripts/doorHealth.cs
+++ Castle Math/Assets/Scripts/doorHealth.cs
@@ -5,19 +5,19 @@ using UnityEngine.UI;
 
 public class doorHealth : MonoBehaviour {
 	public Text doorHealthText;
-
+	public bool destroyed = false;
 	public int Health = 100;
-	
+
 	private gameStateManager GameManager;
 	private ExampleGUIAspectsController doorHealthBar;
 
 	// Use this for initialization
 	void Start () {
-			GameManager = GameObject.FindObjectOfType<gameStateManager> ();
-			doorHealthBar = GameObject.FindObjectOfType<ExampleGUIAspectsController> ();
+		GameManager = GameObject.FindObjectOfType<gameStateManager> ();
+		doorHealthBar = GameObject.FindObjectOfType<ExampleGUIAspectsController> ();
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if(Health<1) GameManager.LoseState ();
@@ -28,5 +28,11 @@ public class doorHealth : MonoBehaviour {
 	public void TakeDamageGate(int damage) {
 		doorHealthBar.updateHealth(damage);
 		Health -= damage;
+
+		if (Health < 0) {
+			this.gameObject.GetComponent<Renderer> ().enabled = false;
+			Debug.Log ("destroyed dh");
+			this.destroyed = true;
+		}
 	}
 }
