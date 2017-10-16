e5233c2d46dd4d75ec846043c4d9a12783db8414
contrejo27 <contrejo27@gmail.com>
2017-10-16 15:37:02 -0400

    tuned multiple choice UI

--- Castle Math/Assets/Scripts/doorHealth.cs
+++ Castle Math/Assets/Scripts/doorHealth.cs
@@ -32,7 +32,6 @@ public class doorHealth : MonoBehaviour {
 		if (Health < 0) {
 			this.gameObject.GetComponent<Renderer> ().enabled = false;
 			Debug.Log ("destroyed dh");
-			this.destroyed = true;
 		}
 	}
 }
