51cbc2adde534ab3d17c4f3bc16f553583903451
jose contreras <contrejo27@gmail.com>
2018-04-16 11:20:43 -0400

    reverted wavemanager to before tunable waves to prevent crash

--- Castle Math/Assets/Scripts/Enemy/WaveManager.cs
+++ Castle Math/Assets/Scripts/Enemy/WaveManager.cs
@@ -10,6 +10,7 @@ public class WaveManager : MonoBehaviour {
 	public GameObject KnightPrefab;
 	public GameObject trollPrefab;
 	public GameObject horseRiderPrefab;
+	public int firstWaveSize = 1;
 	private int WaveSize;
 	public int currentWave = 0;
 	
@@ -32,78 +33,10 @@ public class WaveManager : MonoBehaviour {
 	public TutorialBehavior interMath;
 	private int CurrentEnemies;
 
-	int[][] footknightWaves = new int[20][];
-	int[][] horseknightWaves = new int[20][];
-	int[][] trollWaves = new int[20][];
-
-
+	
 	// Use this for initialization
 	void Start () {
 		A_Source = GameObject.Find ("CastleAudio").GetComponent<AudioSource> ();
-
-		//first integer in array is type of launch (all at once/staggered/waves) second is number of enemies per lane
-		footknightWaves[0] = new int[] { 0, 1 };
-		footknightWaves[1] = new int[] { 2, 2 };
-		footknightWaves[2] = new int[] { 2, 3 };
-		footknightWaves[3] = new int[] { 0, 4 };
-		footknightWaves[4] = new int[] { 2, 3 };
-		footknightWaves[5] = new int[] { 2, 3 };
-		footknightWaves[6] = new int[] { 2, 3 };
-		footknightWaves[7] = new int[] { 2, 3};
-		footknightWaves[8] = new int[] { 2, 3};
-		footknightWaves[9] = new int[] { 1, 5};
-		footknightWaves[10] = new int[] { 0, 4};
-		footknightWaves[11] = new int[] { 2, 4};
-		footknightWaves[12] = new int[] { 1, 3};
-		footknightWaves[13] = new int[] { 0, 3};
-		footknightWaves[14] = new int[] { 2, 5 };
-		footknightWaves[15] = new int[] { 1, 4};
-		footknightWaves[16] = new int[] { 2, 4};
-		footknightWaves[17] = new int[] { 0, 8};
-		footknightWaves[18] = new int[] { 1, 4};
-		footknightWaves[19] = new int[] { 2, 6};
-
-		horseknightWaves[0] = new int[] { 0, 0 };
-		horseknightWaves[1] = new int[] { 0, 0 };
-		horseknightWaves[2] = new int[] { 0, 0 };
-		horseknightWaves[3] = new int[] { 0, 0 };
-		horseknightWaves[4] = new int[] { 0, 1 };
-		horseknightWaves[5] = new int[] { 1, 1 };
-		horseknightWaves[6] = new int[] { 1, 1 };
-		horseknightWaves[7] = new int[] { 0, 0};
-		horseknightWaves[8] = new int[] { 1, 2};
-		horseknightWaves[9] = new int[] { 3, 1};
-		horseknightWaves[10] = new int[] { 1, 0};
-		horseknightWaves[11] = new int[] { 1, 3};
-		horseknightWaves[12] = new int[] { 1, 0};
-		horseknightWaves[13] = new int[] { 1, 0};
-		horseknightWaves[14] = new int[] { 0, 2};
-		horseknightWaves[15] = new int[] { 0, 3};
-		horseknightWaves[16] = new int[] { 0, 1};
-		horseknightWaves[17] = new int[] { 0, 0};
-		horseknightWaves[18] = new int[] { 3, 3};
-		horseknightWaves[19] = new int[] { 2, 2};
-
-		trollWaves[0] = new int[] { 0, 0 };
-		trollWaves[1] = new int[] { 0, 0 };
-		trollWaves[2] = new int[] { 0, 0 };
-		trollWaves[3] = new int[] { 0, 0 };
-		trollWaves[4] = new int[] { 0, 0 };
-		trollWaves[5] = new int[] { 0, 0 };
-		trollWaves[6] = new int[] { 1, 0 };
-		trollWaves[7] = new int[] { 0, 0};
-		trollWaves[8] = new int[] { 1, 0};
-		trollWaves[9] = new int[] { 2, 0};
-		trollWaves[10] = new int[] { 1, 0};
-		trollWaves[11] = new int[] { 1, 1};
-		trollWaves[12] = new int[] { 1, 0};
-		trollWaves[13] = new int[] { 1, 2};
-		trollWaves[14] = new int[] { 1, 1};
-		trollWaves[15] = new int[] { 1, 1};
-		trollWaves[16] = new int[] { 1, 2};
-		trollWaves[17] = new int[] { 1, 0};
-		trollWaves[18] = new int[] { 1, 0};
-		trollWaves[19] = new int[] { 0, 3};
     }
 
     public void NextWave()
@@ -123,15 +56,11 @@ public class WaveManager : MonoBehaviour {
 
 	public void ActivateWave(int WaveIndex)
 	{
+		//the wave size increases by two men each wave
+		WaveSize = firstWaveSize + WaveIndex;
 		setWaveText();
 		//Create all of the enemies
-		if(currentWave != 0){ // add && currentWave < 2
-			StartCoroutine (announceWave());
-		}
-		StartCoroutine (ActivateEnemies(footknightWaves, KnightPrefab,null));
-		StartCoroutine (ActivateEnemies(trollWaves, trollPrefab, trollSpawnSound));
-		StartCoroutine (ActivateEnemies(horseknightWaves, horseRiderPrefab, horseRiderSpawnSound));
-
+		StartCoroutine (ActivateEnemies(WaveSize));
 	}
 
 	void setWaveText(){
@@ -140,58 +69,15 @@ public class WaveManager : MonoBehaviour {
 		StartCoroutine(FadeOut(waveEffect));
 	}
 	
-	IEnumerator announceWave(){
-		yield return new WaitForSeconds (4f);
-			A_Source.clip = AnotherWave;
-			A_Source.Play ();
-	}	
-
-	IEnumerator ActivateEnemies(int[][] waveType, GameObject enemyPrefab,AudioClip spawnSound)
+	IEnumerator ActivateEnemies(int WaveSize)
 	{		
 		//delay so the player can breather/ do math
-
-		yield return new WaitForSeconds (2f);
-		//if this wave is all at once
-		if(waveType[currentWave][0] == 0)
-		{
-			for (int i = 0; i <  waveType[currentWave][1]; i++) {
-				spawnEnemy(enemyPrefab, spawnSound,0);
-				yield return new WaitForSeconds (.1f);
-				spawnEnemy(enemyPrefab, spawnSound,1);
-				yield return new WaitForSeconds (.1f);
-				spawnEnemy(enemyPrefab, spawnSound,2);
-				yield return new WaitForSeconds (Random.Range (.1f, .2f));
-			}
+		yield return new WaitForSeconds (7f);
+		if(currentWave != 0){ // add && currentWave < 2
+		A_Source.clip = AnotherWave;
+		A_Source.Play ();
 		}
-
-
-		//if this wave is staggered
-		if(waveType[currentWave][0] == 1)
-		{
-			for (int i = 0; i <  waveType[currentWave][1]; i++) {
-				spawnEnemy(enemyPrefab, spawnSound,0);
-				yield return new WaitForSeconds (Random.Range (0.7f, 1.5f));
-				spawnEnemy(enemyPrefab, spawnSound,1);
-				yield return new WaitForSeconds (Random.Range (0.7f, 1.5f));
-				spawnEnemy(enemyPrefab, spawnSound,2);
-				yield return new WaitForSeconds (.2f);
-			}
-		}
-
-		//if this wave is waves
-		if(waveType[currentWave][0] == 2)
-		{
-			for (int i = 0; i <  waveType[currentWave][1]; i++) {
-				spawnEnemy(enemyPrefab, spawnSound,0);
-				yield return new WaitForSeconds (Random.Range (.05f, .1f));
-				spawnEnemy(enemyPrefab, spawnSound,1);
-				yield return new WaitForSeconds (Random.Range (.05f, .1f));
-				spawnEnemy(enemyPrefab, spawnSound,2);
-				yield return new WaitForSeconds (3.5f);
-			}
-		}
-
-			/*
+			
 		SetNumberOfEnemies (WaveSize);
 		for (int i = 0; i <  WaveSize; i++) {
 			spawnEnemy(KnightPrefab, null);
@@ -209,7 +95,6 @@ public class WaveManager : MonoBehaviour {
             }
         }
 
-
         if (currentWave % 4 == 0 && currentWave != 0)
         {
             for (int i = 0; i < currentWave / 3; i++)
@@ -221,7 +106,7 @@ public class WaveManager : MonoBehaviour {
             }
         }
 		
-		if(currentWave > 12){
+		if(currentWave > 15){
 		    spawnEnemy(horseRiderPrefab, horseRiderSpawnSound);
 			addEnemyToWaveSize();
 		}
@@ -230,7 +115,7 @@ public class WaveManager : MonoBehaviour {
 		    spawnEnemy(trollPrefab, trollSpawnSound);
 			addEnemyToWaveSize();
 		}
-		*/
+		
 		//leave the title up for another second
 		yield return new WaitForSeconds (1);
 
@@ -250,21 +135,17 @@ public class WaveManager : MonoBehaviour {
 	}
 
 	//spawns enemy at a random spawn point
-	void spawnEnemy(GameObject enemy, AudioClip spawnSound,int spawn){
+	void spawnEnemy(GameObject enemy, AudioClip spawnSound){
 		int randomSpawn = Random.Range(0, SpawnPoints.Length);
-		GameObject enemyObject = Instantiate(enemy, SpawnPoints[spawn].position + new Vector3(Random.Range(-15, 10), 0,0), SpawnPoints[0].rotation);
-		enemyObject.GetComponent<EnemyBehavior>().SetTarget(spawn+1); //points enemy at right target. adjusted for UI name
-		addEnemyToWaveSize();
-					//GameObject enemyObject = Instantiate(enemy, SpawnPoints[randomSpawn].position+ new Vector3(Random.Range(-15, 10), 0,0), SpawnPoints[randomSpawn].rotation);
-
-
+		GameObject enemyObject = Instantiate(enemy, SpawnPoints[randomSpawn].position+ new Vector3(Random.Range(-15, 10), 0,0), SpawnPoints[randomSpawn].rotation);
+		enemyObject.GetComponent<EnemyBehavior>().SetTarget(randomSpawn + 1); //adjusted for UI name
+	
 		if(spawnSound != null){
 			enemySounds.clip = spawnSound;
 			enemySounds.Play ();
 		}
 	}
 	
-
 	public void EnemyKilled()
 	{
 		CurrentEnemies -= 1;
--- Castle Math/Assets/Scripts/Math/AnswerInput.cs
+++ Castle Math/Assets/Scripts/Math/AnswerInput.cs
@@ -65,7 +65,6 @@ public class AnswerInput : MonoBehaviour {
 	public void ClearAnswer()
 	{
 		//AnswerText.text = "";
-
 	}
 
 	/// <summary>
