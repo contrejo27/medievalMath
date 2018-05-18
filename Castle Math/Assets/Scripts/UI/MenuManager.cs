87a37b26310180d4068e1250d4a31acf781d51c4
jose contreras <contrejo27@gmail.com>
2018-05-18 13:24:55 -0400

    added level select after stat screen

---
+++ Castle Math/Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.VR;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public GameObject loginPopup;
+    public Animator mathMenu;
+    public GameObject mathSelectMenu;
+    public GameObject lessonSelectOptions;
+    public GameObject detailedViewOptions;
+    public Text instructions;
+
+    public AudioSource UIAudio;
+    public AudioClip splashScreenSountrack;
+
+    public Button m_loginButton;
+    public mathController mController;
+
+    private void Start()
+    {
+        UIAudio.clip = splashScreenSountrack;
+        UIAudio.Play();
+        print("Logged in " + PlayerPrefs.GetInt("LoggedIn"));
+    }
+
+    private void Update()
+    {
+        if (PlayerPrefs.GetInt("LoggedIn") == 1)
+        {
+            m_loginButton.interactable = false;
+            m_loginButton.transform.GetChild(0).GetComponent<Text>().text = "Logged In";
+        }
+        else
+        {
+            m_loginButton.interactable = true;
+            m_loginButton.transform.GetChild(0).GetComponent<Text>().text = "Log In";
+        }
+    }
+
+    public void loadGame()
+    {
+        if (PlayerPrefs.GetInt("LoggedIn") == 1)
+        {
+            mController.StartGame();
+        }
+        else
+        {
+            OpenLoginPopup();
+        }
+    }
+
+    public void OpenLoginPopup()
+    {
+        if (loginPopup)
+        {
+            GameObject newLoginPopup = Instantiate(loginPopup, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+        else
+            Debug.LogError("No loginPopup attached on the menuManager object");
+    }
+
+    public void OpenMathMenu()
+    {
+        mathMenu.Play("slideIn");
+    }
+
+    public void CloseMathMenu()
+    {
+        mathMenu.Play("slideOut");
+    }
+
+    public void OpenMathSelectMenu()
+    {
+        mathSelectMenu.SetActive(true);
+        mathSelectMenu.GetComponent<Animator>().Play("slideIn");
+    }
+
+    public void CloseMathSelectMenu()
+    {
+        mathSelectMenu.GetComponent<Animator>().Play("slideOut");
+        StartCoroutine(hideAfterWait());
+    }
+
+    IEnumerator hideAfterWait()
+    {
+        yield return new WaitForSeconds(.3f);
+        mathSelectMenu.SetActive(false);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void updateMathMenu(string menuSelection)
+    {
+        if (menuSelection == "detailView")
+        {
+            instructions.text = "Scroll to view all results";
+            lessonSelectOptions.SetActive(false);
+            detailedViewOptions.SetActive(true);
+
+        }
+        if (menuSelection == "lessonSelect")
+        {
+            instructions.text = "Select in-game lessons";
+            detailedViewOptions.SetActive(false);
+            lessonSelectOptions.SetActive(true);
+        }
+    }
+
+
+}
---
+++ Castle Math/Assets/Scripts/UI/ShootingUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingUI : MonoBehaviour {
+
+	public TutorialBehavior tutorial;
+
+	// Use this for initialization
+	public void Select () {
+		tutorial.Next();
+	}
+
+	void OnCollisionEnter(Collision otherCollision)
+	{
+		Select(); 
+	}
+
+}
