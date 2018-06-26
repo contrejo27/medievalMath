17858593fb9092df21f50d664d1d4317ee11c3d4
Rennar <jwilliamson@knights.ucf.edu>
2018-06-26 13:45:02 -0400

    lemme pull

---
+++ Castle Math/Assets/Scripts/Interactable Objects/Potions/SlowTimePotion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowTimePotion : Potion {
+
+    protected override void Init()
+    {
+        toolTip.text = "Slow Time Potion";
+        base.Init();
+    }
+
+    public override void DoEffect()
+    {
+        GameStateManager.instance.SetTimeScale(.50f, 8f);
+        base.DoEffect();
+    }
+}
