0b7b98842f31891679ddd378072e758d6760d191
Daniel Selvia <selviadaniel94@gmail.com>
2018-07-02 16:13:29 -0400

    Desert Level Terrain Generated, Camera Set Up and Spawn Points

---
+++ Castle Math/Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour {
+    public int depth = 20;
+
+    public int height = 256;
+    public int width = 256;
+    public float scale = 20f;
+    // Use this for initialization
+    void Start() {
+       
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+    }
+
+    TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width +1;
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, GenerateHeights());
+        return terrainData;
+    }
+
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y);
+            }
+        }
+        return heights;
+}
+    float CalculateHeight(int x,int y)
+    {
+        float xCoord = (float) x / height * scale;
+        float yCoord = (float) y / height * scale;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
