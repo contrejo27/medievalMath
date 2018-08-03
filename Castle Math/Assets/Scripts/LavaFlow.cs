3d8838c7dc4fa85e0c213de52d58f7100a39e59b
Daniel Selvia <selviadaniel94@gmail.com>
2018-08-02 21:23:22 -0400

    Lava FUnctionality Progress

---
+++ Castle Math/Assets/Scripts/LavaFlow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaFlow : MonoBehaviour {
+    public float rotateSpeed = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Renderer>().material.SetFloat("_Rotation", Time.time * rotateSpeed);
+    }
+}
