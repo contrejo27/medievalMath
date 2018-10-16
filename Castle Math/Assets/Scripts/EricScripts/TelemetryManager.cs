b9ab0f36502efbab229af3b7049a208d65a8218c
Eu <emej92@gmail.com>
2018-10-15 20:14:29 -0400

    Created Amazon DynamoDB Module
    - Removed all the database components from the TelemetryManager and moved them to the AmazonDynamoDBModule for better modularity.
    - It only reads and writes to a single element in the database based on the username because uniqueIds have not been implemented.

--- Castle Math/Assets/Scripts/EricScripts/TelemetryManager.cs
+++ Castle Math/Assets/Scripts/EricScripts/TelemetryManager.cs
@@ -15,36 +15,7 @@ using Amazon.DynamoDBv2.Model;
 public class TelemetryManager : MonoBehaviour {
     public static TelemetryManager instance;
 
-    private CognitoAWSCredentials m_credentials;
-    private IAmazonDynamoDB client;
-    private DynamoDBContext m_context;
-
-    private DynamoDBContext DBContext {
-        get {
-            if (m_context == null)
-                m_context = new DynamoDBContext(client);
-            return m_context;
-        }
-    }
-
-    public const string METRIC_WAVELOST = "WaveLost";
-    public const string METRIC_TIMEINVR = "TimeInVR";
-    public const string METRIC_ARROWSLEFT = "ArrowsLeft";
-    public const string TABLE_NAME = "GameMetricsData";
-    public const string DB_URL = "us-east-1:30e8de39-1567-46d7-a033-02870c1d2102";
-
-    [DynamoDBTable(TABLE_NAME)]
-    public class MetricSchema {
-        // TODO: Add User
-        [DynamoDBHashKey]
-        public string Metric { get; set; }
-        [DynamoDBProperty]
-        public float Value { get; set; }
-        [DynamoDBProperty]
-        public int NumberEntries { get; set; }
-    }
-
-    private List<MetricSchema> m_Metrics = new List<MetricSchema>();
+    private AmazonDynamoDBModule DB;
 
     private void Awake() {
         instance = this;
@@ -53,132 +24,33 @@ public class TelemetryManager : MonoBehaviour {
     }
 
     private void Start() {
-        client = CreateDBClient();
+        DB = new AmazonDynamoDBModule();
     }
 
-    AmazonDynamoDBClient CreateDBClient() {
-        Amazon.AWSConfigs.HttpClient = Amazon.AWSConfigs.HttpClientOption.UnityWebRequest;
-        m_credentials = new CognitoAWSCredentials(DB_URL, Amazon.RegionEndpoint.USEast1);
-
-        var ddbClient = new AmazonDynamoDBClient(m_credentials, Amazon.RegionEndpoint.USEast1);
-
-        m_credentials.GetIdentityIdAsync(delegate (AmazonCognitoIdentityResult<string> result) {
-
-            var request = new DescribeTableRequest {
-                TableName = @TABLE_NAME
-            };
-
-            ddbClient.DescribeTableAsync(request, (ddbresult) => {
-                if (result.Exception != null) {
-                    Debug.Log(result.Exception.Message);
-                    return;
-                }
-
-                var response = ddbresult.Response;
-
-                TableDescription description = response.Table;
-                Debug.Log("Table Name: " + description.TableName);
-                Debug.Log("# of Items: " + description.ItemCount);
-                Debug.Log("Schema: " + description.KeySchema[0].AttributeName);
-
-            }, null);
-
-            client = ddbClient;
-            GetMetric(METRIC_TIMEINVR);
-        });
-        return ddbClient;
-    }
-
-
-    private void Update() {
+    private void TestDB() {
         /// <summary>
-        /// TODO: Confirm
-        /// Check which key is being pressed and updates the corresponding metric
+        /// Check which key is being pressed and updates the corresponding metric on the database
         /// </summary>
-        // Number row 1
+
+        // Number row above QWERTY
         if(Input.GetKeyDown(KeyCode.Alpha1)) {
             Debug.Log("Pressed 1");
-            PostMetric(METRIC_ARROWSLEFT, Random.Range(1,5));
+            DB.CreateMetric("PressedKey1", Random.Range(1,5));
         }
         if (Input.GetKeyDown(KeyCode.Alpha2)) {
             Debug.Log("Pressed 2");
-            PostMetric(METRIC_TIMEINVR, Random.Range(1, 5));
+            DB.CreateMetric("PressedKey2", Random.Range(1, 5));
         }
         if (Input.GetKeyDown(KeyCode.Alpha3)) {
             Debug.Log("Pressed 3");
-            PostMetric(METRIC_WAVELOST, Random.Range(1, 5));
+            DB.CreateMetric("PressedKey3", Random.Range(1, 5));
         }
     }
 
-    private void GetMetric(string metric) {
-
-        Table.LoadTableAsync(client, TABLE_NAME, (result) => {
-            if (result.Exception != null) {
-                Debug.Log("Failed to load Metric Table");
-            }
-            else {
-                Debug.Log("Result succesful");
-                try {
-                    Debug.Log("Trying to scan");
-
-                    var search = DBContext.ScanAsync<MetricSchema>(new ScanCondition("Metric", ScanOperator.NotContains, "null"));
-                    Debug.Log("Done Scanning");
-
-
-                    search.GetRemainingAsync(scanResult => {
-                        if (scanResult.Exception == null) {
-                            Debug.Log("Scan Result complete");
-			    m_Metrics = scanResult.Result;
-			    foreach (MetricSchema item in m_Metrics) {
-				Debug.Log("Name  Value  Entries\n" + item.Metric + " " + item.Value + " " + item.NumberEntries);
-			    }
-                        }
-                        else {
-                            Debug.Log("Failed to get async table scan results: " + scanResult.Exception.Message);
-                        }
-                    }, null);
-                }
-                catch (AmazonDynamoDBException exception) {
-                    Debug.Log(string.Concat("Exception fetching characters from table: {0}", exception.Message));
-                    Debug.Log(string.Concat("Error code: {0}, error type: {1}", exception.ErrorCode, exception.ErrorType));
-                }
-            }
-        });
-    }
-
-    public void PostMetric(string metric, float modifier) {
+    private void Update() {
         /// <summary>
-        /// Post metrics to the database.
+        /// Unity Function that runs after every screen update
         /// </summary>
-        foreach (MetricSchema data in m_Metrics) {
-            if (data.Metric == metric) {
-                float newAverage;
-                if (data.NumberEntries == 0) {
-                    newAverage = modifier;
-                }
-                else {
-                    float currentAverage = data.Value * data.NumberEntries;
-                    newAverage = (currentAverage + modifier) / (data.NumberEntries + 1);
-                }
-
-                MetricSchema newMetric = new MetricSchema {
-                    Metric = metric,
-                    NumberEntries = data.NumberEntries + 1,
-                    Value = newAverage,
-                };
-
-                DBContext.SaveAsync(newMetric, (result) => {
-                    if (result.Exception == null) {
-                        Debug.Log("Metric updated!");
-                        m_Metrics.Clear();
-                        GetMetric(metric);
-                    }
-                    else
-                        Debug.Log(result.Exception);
-                });
-            }
-        }
+        TestDB();
     }
-
-
 }
