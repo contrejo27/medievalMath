ca90216107ec54e352f60ef965f490065be0714e
humanMusic <contrejo27@gmail.com>
2018-11-01 17:24:52 -0400

    added fraction reduction, fixed math select UI

--- Castle Math/Assets/Resources/waves/readme.txt
+++ Castle Math/Assets/Resources/waves/readme.txt
@@ -7,3 +7,5 @@ Wave types.
 1 staggered. come out one at a time for a longer period of time. multiply by 3
 2 waves. come in waves of 3 every certain 
 3 singles. one at a time
+
+
--- Castle Math/Assets/Scripts/EricScripts/TelemetryManager.cs
+++ Castle Math/Assets/Scripts/EricScripts/TelemetryManager.cs
@@ -9,7 +9,6 @@ using UnityEngine;
 public class TelemetryManager : MonoBehaviour {
     public static TelemetryManager instance;
 
-    private static SQLDBModule DB;
 
     // Variables to keep database details private and outside git
     public string DB_URL;
@@ -221,109 +220,4 @@ public class TelemetryManager : MonoBehaviour {
     public void LogSession() {
         APIPost("session", SessionPayload());
     }
-
-
-
-
-    //////////////
-    // DATABASE //
-    //////////////
-    
-    // TODO: Remove DB from telemetry, not needed anymore after API implementation
-    // TODO: Move Table making functions to separate file
-
-    private void TestDB() {
-        /// <summary>
-        /// Check which key is being pressed and updates the corresponding metric on the database
-        /// </summary>
-
-        // Number row above QWERTY
-        if(Input.GetKeyDown(KeyCode.Alpha1)) {
-            PrintTable(DB.Select());
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            Write("fakeProperty2", "1");
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3)) {
-            Write("fakeProperty3", 1.2);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha0)) {
-            Debug.Log("DELETING ALL ROWS!!!\nYou have 5 seconds to cancel!!!");
-            System.Threading.Thread.Sleep(5000);
-
-            DB.DeleteAll();
-        }
-    }
-
-    public void Write<T>(string propertyName, T propertyValue) {
-        DB.Insert("eu", "Unity3D", propertyName, propertyValue.ToString());
-    }
-
-    public void SaveTable(List<List<string>> list) {
-        System.IO.File.WriteAllText(@"C:\Users\Home\database.txt", MakeTable(list));
-    }
-
-    public void PrintTable(List<List<string>> list) {
-        Debug.Log(MakeTable(list));
-    }
-
-    public string MakeTable(List<List<string>> list) {
-        string table = "";
-        string header = string.Format("| {0,22} " +
-                                      "| {1,22} " +
-                                      "| {2,22} " +
-                                      "| {3,22} " +
-                                      "| {4,22} " +
-                                      "| {5,22} " +
-                                      "|"
-				                      ,
-                                      "Time",
-                                      "User",
-                                      "Device",
-                                      "Property",
-                                      "Value",
-                                      "Modified"
-        );
-
-        foreach (List<string> row in list) {
-            table += string.Format("| {4,22} " +
-                                   "| {0,22} " +
-                                   "| {1,22} " +
-                                   "| {2,22} " +
-                                   "| {3,22} " +
-                                   "| {5,22} " +
-                                   "|\n"
-				                   ,
-                                   row[0],
-                                   row[1],
-                                   row[2],
-                                   row[3],
-                                   row[4],
-                                   row[5]
-			);
-        }
-
-        return header + "\n" + table;
-    }
-
-    public void Print(List<string> item) {
-        // Unity only allows 2 lines to be displayed on the console
-        //   Remember to click to read the rest of it
-        Debug.Log(string.Format("Time: {4}\n" +
-                                "User: {0}\n" +
-                                "Device: {1}\n" +
-                                "Property: {2}\n" +
-                                "Value: {3}\n" +
-                                "Modified: {5}"
-				                ,
-                                item[0],
-                                item[1],
-                                item[2],
-                                item[3],
-                                item[4],
-                                item[5]
-				            )
-		);
-    }
-
 }
