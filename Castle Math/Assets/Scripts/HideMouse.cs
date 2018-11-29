ceac69fc61e6ac5ffc9d03203db0b4cd353903f7
Littleweeman <32720425+Littleweeman@users.noreply.github.com>
2018-11-29 11:25:16 -0500

    potion resize/mouse hidden/forst boat texture readded

---
+++ Castle Math/Assets/Scripts/HideMouse.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorScript : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+		//Set Cursor to not be visible
+		Cursor.visible = false;
+	}
+}
