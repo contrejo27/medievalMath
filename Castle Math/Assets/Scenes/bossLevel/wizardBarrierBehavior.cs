2954c2d63f7b70fb72d81e4ab82a0a150274f181
Eu <emej92@gmail.com>
2018-12-06 15:40:57 -0500

    initial boss animation

---
+++ Castle Math/Assets/Scenes/bossLevel/wizardBarrierBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wizardBarrierBehavior : MonoBehaviour {
+
+    Animator m_Animator;
+    string animationName = "wizardBarrierPop";
+
+    void Start() {
+        m_Animator = GetComponent<Animator>();
+        m_Animator.SetFloat("Direction", 1.0f);
+    }
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.Space)) {
+            Toggle();
+        }
+        if (Input.GetKey(KeyCode.Alpha1)) {
+            Rise();
+        }
+        if (Input.GetKey(KeyCode.Alpha2)) {
+            Sink();
+        }
+    }
+
+
+    private void Toggle() {
+        /// <summary>
+        /// Summons/returns the rock underground
+        /// </summary>
+
+        m_Animator.SetFloat("Direction", (-1.0f) * m_Animator.GetFloat("Direction"));
+        m_Animator.Play(animationName);
+    }
+
+    private void Rise() {
+        /// <summary>
+        /// Summons the rock from underground
+        /// </summary>
+        m_Animator.SetFloat("Direction", 1.0f);
+        m_Animator.Play(animationName);
+    }
+
+    private void Sink() {
+        /// <summary>
+        /// Returns the rock underground
+        /// </summary>
+        m_Animator.SetFloat("Direction", -1.0f);
+        m_Animator.Play(animationName);
+    }
+}
