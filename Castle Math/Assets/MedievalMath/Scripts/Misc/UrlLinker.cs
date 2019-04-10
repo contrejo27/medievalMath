8cf351b776373819c94301908cd4c40f731db714
Jose <contrejo27@gmail.com>
2019-04-10 16:14:50 -0400

    updated intro UI

---
+++ Castle Math/Assets/MedievalMath/Scripts/Misc/UrlLinker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UrlLinker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void goToURL(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
